e7e9283a5946188b84cdb5aa583e8761e5985288
Akshay <Akshay@Akshay>
2022-01-26 00:00:43 +0530

    Second Commit

---
+++ M06/src/ArithmeticOperators.java
@@ -0,0 +1,20 @@
+
+public class ArithmeticOperators 
+{
+
+
+	public static void main(String[] args) 
+	{
+		
+		int x = 10, y=20;
+		System.out.println(x+y);		// addition 
+		System.out.println(--x);		// Pre-increment
+		System.out.println(++y);		// Pre-increment   	// Post-increment
+
+		
+		System.out.println(x--);		// Post-increment
+		System.out.println(y++);		// Post-increment
+
+	}
+
+}
---
+++ M06/src/SwitchCondition.java
@@ -0,0 +1,29 @@
+
+public class SwitchCondition {
+
+	public static void main(String[] args) {
+
+
+		for(int i = 1; i <= 5; i++)
+		{
+			switch (i) 
+			{
+			case 1: 
+				System.out.println("The value of x is 1");
+				break;
+			case 2:
+				System.out.println("The value of x is 2");
+				break;
+			case 3:
+				System.out.println("The value of x is 3");
+				break;
+			default:
+			System.out.println("The value of x is other than 1,2,3");
+				break;
+
+			}
+
+		}
+	}
+
+}
